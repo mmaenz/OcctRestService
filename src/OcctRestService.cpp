2d3f863b9ebb9d631b5aaff5bac79be032ec8be9
Michael Maenz <mmaenz@yahoo.de>
2019-10-01 22:42:07 +0200

    Working

---
+++ src/OcctRestService.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : OcctRestService.cpp
+// Author      : Michael Maenz
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	return 0;
+}
